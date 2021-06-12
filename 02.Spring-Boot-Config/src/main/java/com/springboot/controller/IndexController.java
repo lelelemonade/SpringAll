b1fa4d284d0dd065eead9e438aa94842bc1f3b3b
LeLeLemonade <szl921818@gmail.com>
2021-06-12 17:05:43 +0800

    1、2、3

--- 02.Spring-Boot-Config/src/main/java/com/springboot/controller/IndexController.java
+++ 02.Spring-Boot-Config/src/main/java/com/springboot/controller/IndexController.java
@@ -1,25 +1,24 @@
 package com.springboot.controller;
 
+import com.springboot.bean.BlogProperties;
+import com.springboot.bean.ConfigBean;
+import com.springboot.bean.TestConfigBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.springboot.bean.BlogProperties;
-import com.springboot.bean.ConfigBean;
-import com.springboot.bean.TestConfigBean;
-
 
 @RestController
 public class IndexController {
-	@Autowired
-	private BlogProperties blogProperties;
-	@Autowired
-	private ConfigBean configBean;
-	@Autowired
-	private TestConfigBean testConfigBean;
-	
-	@RequestMapping("/")
-	String index() {
-		return testConfigBean.getName()+"，"+testConfigBean.getAge();
-	}
+    @Autowired
+    private BlogProperties blogProperties;
+    @Autowired
+    private ConfigBean configBean;
+    @Autowired
+    private TestConfigBean testConfigBean;
+
+    @RequestMapping("/")
+    String index() {
+        return testConfigBean.getName() + "，" + testConfigBean.getAge();
+    }
 }
